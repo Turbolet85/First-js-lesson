ed33fda29e0b7c49ef6e8a76e7540ba57ea2b4bb
Anton Soldatov <turbolet85@gmail.com>
2022-08-13 08:57:21 +0200

    js first lessons to repo

---
+++ js/script.js
@@ -0,0 +1,126 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// {
+//     let result = 50;
+//     console.log(result);
+// }
+
+
+
+
+
+
+// data types
+
+// let number = 4.6;
+
+// console.log(4/0);
+
+// console.log('string' * 9);
+
+// const person = "Alex";
+
+// const bool = true;
+
+// let und;
+// console.log(und);
+
+//objects
+// const obj = {
+// 	name: "John",
+// 	age: 25,
+// 	isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+//Array
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+
+
+
+//Simple interactions with page
+
+// alert('Hello');
+
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+
+// const answer = +prompt("Are you 18?", "18");
+// console.log(answer + 5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('Whats your name?', '');
+// answers[1] = prompt('Whats your surname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// console.log(answers);
+// document.write(answers);
+
+
+
+
+//Interpolation
+
+// const category = 'toys';
+// console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Hello ${user}`);
+
+
+
+
+//Operators
+
+// console.log('arr' + ' - object');
+// console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+console.log(2*4 === '8');
+
+const isChecked = true;
+const isClose = false;
+
+// console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+
